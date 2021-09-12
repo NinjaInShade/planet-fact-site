20c955d4afa89b163ecf4b7405e380b73c2d8a96
Leon Michalak <leonmichalak6@gmail.com>
2021-09-12 13:47:18 +0100

    Added navbar to routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import LoadingSpinner from './components/LoadingSpinner/LoadingSpinner';
+import Navbar from './components/Navbar/Navbar.js';
 import './base.css';
 
 const PlanetPage = lazy(() => import('./pages/PlanetPage/PlanetPage'));
@@ -9,6 +10,9 @@ function App() {
   return (
     <Router>
       <Suspense fallback={<LoadingSpinner />}>
+        {/* Navbar needs to be on every page */}
+        <Navbar />
+
         <Switch>
           {/* Entry page for application - the home page */}
           <Route path='/:planet' exact>
